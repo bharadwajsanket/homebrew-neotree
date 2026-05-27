07eb95cbf997dd0d06ffd5875beb9f77447194cf
Sanket Bharadwaj <bharadwajsanket@users.noreply.github.com>
2026-05-28 01:47:28 +0530

    docs/install: add UNIX manpage support

--- Formula/neotree.rb
+++ Formula/neotree.rb
@@ -2,7 +2,7 @@ class Neotree < Formula
   desc "Fast directory tree CLI with glob filtering, sorting, and export"
   homepage "https://github.com/bharadwajsanket/neotree"
   url "https://github.com/bharadwajsanket/neotree/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "8021b959e246aa497d1c1ca0ee8a0897170783e9fd8abeaf5984ed1509a7c4da"
+  sha256 "ebb961b2a0378b75279596b51a9d1f42a0b11aaf99bc2f2435f25c902bebc128"
   license "GPL-3.0-only"
   head "https://github.com/bharadwajsanket/neotree.git", branch: "main"
 
@@ -10,6 +10,8 @@ class Neotree < Formula
     system "make"
     bin.install "neotree"
     bin.install_symlink "neotree" => "ntree"
+    man1.install "man/neotree.1"
+    man1.install_symlink "neotree.1" => "ntree.1"
   end
 
   test do
