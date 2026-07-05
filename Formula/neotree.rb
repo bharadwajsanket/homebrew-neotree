c651a4e8eb04dcc32c9c8c2dc88e9403a09d2ba8
Sanket Bharadwaj <bharadwajsanket@users.noreply.github.com>
2026-07-06 03:27:14 +0530

    fix(homebrew): update v2.5.4 source checksum

--- Formula/neotree.rb
+++ Formula/neotree.rb
@@ -2,7 +2,7 @@ class Neotree < Formula
   desc "Traversal by Instinct - fast directory tree CLI"
   homepage "https://github.com/bharadwajsanket/neotree"
   url "https://github.com/bharadwajsanket/neotree/archive/refs/tags/v2.5.4.tar.gz"
-  sha256 "f9a6d90984bd9705b1df42e77c321c8ef026e70ac498d9dc486c53e0bf7f492b"
+  sha256 "2a11feeabf81a46e7a91cb84513e7c25c58eff03d0b25449acebf0e9f84e8051"
   license "GPL-3.0-only"
   head "https://github.com/bharadwajsanket/neotree.git", branch: "main"
 
